bca74947a878531899ce7fb94736010a0ba550a7
Reece Chimento <reecechimento@gmail.com>
2022-02-11 02:33:37 -0600

    (chore): add basic configuration files

---
+++ lua/keybinds.lua
@@ -0,0 +1,7 @@
+vim.cmd[[nnoremap <leader>ev :vs ~/.config/nvim/init.lua<CR>]]
+vim.cmd[[nnoremap <leader>sv :PackerSync<CR>]]
+
+vim.cmd[[nnoremap [h :nohl<CR>]]
+
+vim.cmd[[nnoremap gc I-- <ESC>]]
+
